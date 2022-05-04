af23408f37c8ac7f023314508ae0fc41504400df
kru1910 <kjkotia10@gmail.com>
2022-05-04 15:38:04 +0530

    Concepts: Props,Emmitting custom events,Slots, Teleport, CSS

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <teleport to=".modals"  v-if="showModal">
+    <Modal :header="header" theme="sale"  @close="showModalHandler" >
+      <p>Kru Kotia</p>
+    </Modal>
+  </teleport>
+  <p>Welcome!</p>
+  <button @click="showModalHandler">Show Model</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Modal,
+  },
+  data() {
+    return {
+      title: "Hello From Vue!",
+      header: "Welcome!",
+      showModal: false
+    };
+  },
+  methods:{
+    showModalHandler(){
+      this.showModal = !this.showModal;
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
+#app, .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/Modal.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="backdrop" @click="closeModal">
+    <div class="modal" :class="{sale: theme === 'sale'}" >
+      <h2>{{header}}</h2>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  props: ['header', 'text', 'theme'],
+  methods: {
+    closeModal(){
+      this.$emit('close');
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.modal{
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+.backdrop{
+  top: 0;
+  position: fixed;
+  background: rgba(0,0,0,0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal.sale{
+  background: rgb(238, 146, 165);
+}
+</style>
